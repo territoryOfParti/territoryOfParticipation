6fe7066ed83829b6161902e304774a8002d00ff1
territoryOfParti <partshah07@gmail.com>
2025-10-31 11:11:46 +0100

    Add files via upload

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "spn8@spondonit.com";
+    $to = "pdshah77@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
@@ -13,7 +13,7 @@
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Bitmap Photography.";
+    $subject = "Someone is trying to Get in touch with you from your Website.";
 
     $logo = 'img/logo.png';
     $link = '#';
